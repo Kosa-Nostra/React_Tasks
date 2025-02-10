4e97a1693860cc4f16467d0cab1e71cfeafd9002
Ayder <muzdabaev252@gmail.com>
2025-02-10 08:45:53 +0300

    Task 8

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 
 function App() {
-	return <div>
-		<p class='eee'>123</p>
-		<p>React Tasks</p>
-		<p>JSX</p>
-	</div>;
+	return	<ul>
+		<li>1</li>
+		<li>2</li>
+		<li>3</li>
+		<li>4</li>
+		<li>5</li>
+		<li>6</li>
+		<li>7</li>
+		<li>8</li>
+		<li>9</li>
+		<li>10</li>
+		</ul>;
 }
 
 export default App;
