59474e9cf8fceb7c6213d48161a3a583ad146dff
Ayder <muzdabaev252@gmail.com>
2025-02-10 12:55:49 +0300

    Task 27

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React from 'react';
 
 function App() {
 	let text;
-	const isAdmin = true;
-	
-	if (isAdmin) {
-		text = <p>ADMIN</p>;
+	let age = 14;
+	let adult= age >= 18 ? true: false;
+	if(adult){
+		text=<p>adult</p>
+	}else{
+		text=<p>not adult</p>
 	}
-	
 	return <div>
 		{text}
 	</div>;
