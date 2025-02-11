2e89a9a3c6515835aeb1e3929b5b9257e89193ee
Ayder <muzdabaev252@gmail.com>
2025-02-11 08:11:44 +0300

    Task 35#1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
 
 function App() {
-	function showMess(text) {
-		alert(text);
+	function func(event) {
+		console.log(event); // объект с событием
 	}
+	
 	return <div>
-		<button onClick={() => showMess('1')}>act1</button>
-		<button onClick={() => showMess('2')}>act2</button>
-		<button onClick={() => showMess('3')}>act3</button>
+		<button onClick={func}>act</button>
 	</div>;
 }
 export default App;
