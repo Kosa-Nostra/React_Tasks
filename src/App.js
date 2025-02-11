b6b84e8e7eb217085cd366b1376f205f2bd07d49
Ayder <muzdabaev252@gmail.com>
2025-02-11 10:30:32 +0300

    Task 40

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 function App() {
 	const arr = ['a', 'b', 'c', 'd', 'e'];
-	const res = arr.map(function(item){
-		return <li>{item}</li>;
+	const res = arr.map(function(item, index){
+		return <li key={index}>{item}</li>;
 	});
 	return<div>
 		{res}
