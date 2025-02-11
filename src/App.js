f1261e8589891589c2a882dd66499579c73f173c
Ayder <muzdabaev252@gmail.com>
2025-02-11 10:38:53 +0300

    Tasks 41/42

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import React from 'react';
 
 function App() {
 	const users = [
-		{name: 'user1', surn: 'surn1', age: 30},
-		{name: 'user2', surn: 'surn2', age: 31},
-		{name: 'user3', surn: 'surn3', age: 32},
+		{id: 1,name: 'user1', surn: 'surn1', age: 30},
+		{id: 2,name: 'user2', surn: 'surn2', age: 31},
+		{id: 3,name: 'user3', surn: 'surn3', age: 32},
 	];
-	const res= users.map(function(item, index){
-		return <li key={index}>{item.name} {item.surn} {item.age}</li>
+	const res= users.map(function(item,){
+		return <ul key={item.id}>{item.name} {item.surn} {item.age}</ul>
 	});
 	return<div>
 		{res}
