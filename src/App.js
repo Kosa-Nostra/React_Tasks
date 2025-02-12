d5c1e44f013891e109ff3df9ae2b991478c03923
Ayder <muzdabaev252@gmail.com>
2025-02-12 19:04:55 +0300

    Task 59

--- src/App.js
+++ src/App.js
@@ -7,20 +7,39 @@ function id() {
 }
 
 function App() {
-	const [value, setValue] = useState(0);
+	const [value1, setValue1] = useState(0);
+	const [value2, setValue2] = useState(0);
+	const [value3, setValue3] = useState(0);
+	const [value4, setValue4] = useState(0);
+	const [value5, setValue5] = useState(0);
 	
-	function handleChange(event) {
-		setValue(event.target.value);
+	function handleChange1(event) {
+		setValue1(+event.target.value);
 	}
-	function temp(value){
-		return (value-32)*5/9;
+	
+	function handleChange2(event) {
+		setValue2(+event.target.value);
 	}
 
+	function handleChange3(event) {
+		setValue3(+event.target.value);
+	}
+
+	function handleChange4(event) {
+		setValue4(+event.target.value);
+	}
+
+	function handleChange5(event) {
+		setValue5(+event.target.value);
+	}
+	
 	return <div>
-		<p>Введите температуру в Фаренгейтах:</p>
-		<input value={value} onChange={handleChange} />
-		<p>Вы градусах это: </p>
-		<p>{temp(value)}</p>
-	</div>;
+		<input value={value1} onChange={handleChange1} />
+		<input value={value2} onChange={handleChange2} />
+		<input value={value3} onChange={handleChange3} />
+		<input value={value4} onChange={handleChange4} />
+		<input value={value5} onChange={handleChange5} />
+		<p>result: {(value1 + value2 + value3 + value4 + value5)/5}</p>
+	</div>
 }
 export default App;
