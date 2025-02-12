f35fb37f51d38581bb387d5201ea3824b8ff6df2
Ayder <muzdabaev252@gmail.com>
2025-02-12 19:15:35 +0300

    Task 60#1

--- src/App.js
+++ src/App.js
@@ -9,37 +9,32 @@ function id() {
 function App() {
 	const [value1, setValue1] = useState(0);
 	const [value2, setValue2] = useState(0);
-	const [value3, setValue3] = useState(0);
-	const [value4, setValue4] = useState(0);
-	const [value5, setValue5] = useState(0);
+	const [result, setResult] = useState(0);
+	
 	
 	function handleChange1(event) {
-		setValue1(+event.target.value);
+		setValue1(event.target.value);
 	}
 	
 	function handleChange2(event) {
-		setValue2(+event.target.value);
+		setValue2(event.target.value);
+	}
+	
+	function handleClick() {
+		setResult(Number(value1) + Number(value2));
 	}
 
-	function handleChange3(event) {
-		setValue3(+event.target.value);
-	}
-
-	function handleChange4(event) {
-		setValue4(+event.target.value);
-	}
-
-	function handleChange5(event) {
-		setValue5(+event.target.value);
+	function handleClick1() {
+		setResult(Number(value1) * Number(value2));
 	}
 	
 	return <div>
 		<input value={value1} onChange={handleChange1} />
 		<input value={value2} onChange={handleChange2} />
-		<input value={value3} onChange={handleChange3} />
-		<input value={value4} onChange={handleChange4} />
-		<input value={value5} onChange={handleChange5} />
-		<p>result: {(value1 + value2 + value3 + value4 + value5)/5}</p>
-	</div>
+		
+		<button onClick={handleClick}>btn</button>
+		<button onClick={handleClick1}>btn</button>
+		<p>result: {result}</p>
+	</div>;
 }
 export default App;
