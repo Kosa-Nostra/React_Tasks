82a3d398b2780e037515be5f591bfdf386d82542
Ayder <muzdabaev252@gmail.com>
2025-02-13 07:57:34 +0300

    Task 60#4

--- src/App.js
+++ src/App.js
@@ -5,28 +5,25 @@ import { v4 as uuidv4 } from 'uuid';
 function id() {
 	const randomIdsUUID = Array.from({ length: 5 }, () => uuidv4());
 }
-
 function App() {
-	const [date1, setDate1] = useState("");
-	const [date2, setDate2] = useState("");
-	const [difference, setDifference] = useState(null);
+	const [number, setNumber] = useState("");
+	const [sum, setSum] = useState(null);
   
-	const calculateDifference = () => {
-	  if (date1 && date2) {
-		const d1 = new Date(date1);
-		const d2 = new Date(date2);
-		const diffTime = Math.abs(d2 - d1);
-		const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
-		setDifference(diffDays);
-	  }
+	const calculateSum = () => {
+	  const digits = number.split(" ").join("").split("".trim()).map(Number);
+	  const total = digits.reduce((acc, digit) => acc + digit, 0);
+	  setSum(total);
 	};
   
 	return (
 	  <div>
-		<input type="date" value={date1} onChange={(e) => setDate1(e.target.value)} />
-		<input type="date" value={date2} onChange={(e) => setDate2(e.target.value)} />
-		<button onClick={calculateDifference}>Рассчитать разницу</button>
-		{difference !== null && <p>Разница: {difference} дней</p>}
+		<input 
+		  type="text" 
+		  value={number} 
+		  onChange={(e) => setNumber(e.target.value)} 
+		  onBlur={calculateSum} 
+		/>
+		{sum !== null && <p>Сумма цифр: {sum}</p>}
 	  </div>
 	);
   }
