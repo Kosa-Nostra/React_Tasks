881471c785c6f34064405b900490bc8f7d4aee15
Ayder <muzdabaev252@gmail.com>
2025-02-13 17:39:00 +0300

    Task 70

--- src/App.js
+++ src/App.js
@@ -37,11 +37,34 @@ function translit(word){
 	return answer;
 }
 
+function getAvg(arr) {
+	let sum = 0;
+	
+	for (const elem of arr) {
+		sum += +elem;
+	}
+	
+	return sum;
+}
+
 function App() {
-	const [value, setValue] = useState('');
+	const [notes, setNotes] = useState([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+	
+	function changeHandler(index, event) {
+		setNotes([...notes.slice(0, index), event.target.value, ...notes.slice(index + 1)]);
+	}
+	
+	const result = notes.map((note, index) => {
+		return <input
+			key={index}
+			value={note}
+			onChange={event => changeHandler(index, event)}
+		/>;
+	});
 	
 	return <div>
-		<input defaultValue={value} />
+		{result}
+		{getAvg(notes)}
 	</div>;
 }
 
