fe7b6e8d46d58f786c2ef8b0790445c86dc3e61c
Ayder <muzdabaev252@gmail.com>
2025-02-14 18:25:02 +0300

    Task 74#1

--- src/App.js
+++ src/App.js
@@ -1,37 +1,25 @@
 import React, { useState } from 'react';
-import { nanoid } from 'nanoid';
-import { v4 as uuidv4 } from 'uuid'; 
-
-function id() {
-	const randomIdsUUID = Array.from({ length: 5 }, () => uuidv4());
-}
-
 
 function App() {
-	const [notes, setNotes] = useState(["Первый", "Второй", "Третий"]);
-	const [inputValue, setInputValue] = useState("");
+	const [notes, setNotes] = useState([1, 2, 3, 4, 5]);
   
-	const addNote = () => {
-	  if (inputValue.trim() !== "") {
-		setNotes([...notes, inputValue]);
-		setInputValue("");
-	  }
-	};
+	function doSmth(index) {
+	  let copy = [...notes];
+	  copy[index] = copy[index] ** 2;
+	  setNotes(copy);
+	}
   
 	return (
 	  <div>
-		<input 
-		  type="text" 
-		  value={inputValue} 
-		  onChange={(e) => setInputValue(e.target.value)} 
-		/>
-		<button onClick={addNote}>Добавить</button>
 		<ul>
 		  {notes.map((note, index) => (
-			<li key={index}>{note}</li>
+			<li key={index} onClick={() => doSmth(index)}>
+			  {note}
+			</li>
 		  ))}
 		</ul>
 	  </div>
 	);
   }
+  
 export default App;
