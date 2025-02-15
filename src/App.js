c465547449c23e75a95cab7e7728fe508493d492
Ayder <muzdabaev252@gmail.com>
2025-02-15 20:12:07 +0300

    Task 84

--- src/App.js
+++ src/App.js
@@ -15,12 +15,12 @@ function App() {
 		{id: id(), name: 'user3', surn: 'surn3', age: 32},
 	];
 
+	const result = users.map(user => {
+		return <User key={users.id} name={users[0].name} surn={users[0].surn} age={users[0].age} />
+})
+
 	return <div>
-		<table>
-			<tr><User name={users[0].name} surn={users[0].surn} age={users[0].age} /></tr>
-			<tr><User name={users[1].name} surn={users[1].surn} age={users[1].age} /></tr>
-			<tr><User name={users[2].name} surn={users[2].surn} age={users[2].age} /></tr>
-		</table>
+		{result}
 	</div>;
 }
 
