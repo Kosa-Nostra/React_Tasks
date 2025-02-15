45eca5eed71dd00601e320cbf443e2c6991ccd7e
Ayder <muzdabaev252@gmail.com>
2025-02-15 21:04:41 +0300

    Task 88

--- src/User.js
+++ src/User.js
@@ -1,11 +1,14 @@
 import React from "react";
 
-const User = ({ user, onBan }) => {
+const User = ({ id, name, surname, age, isEdit, toggleMode, editUser }) => {
   return (
-    <p>
-      {user.name} {user.surname}, возраст: {user.age}
-      <button onClick={() => onBan(user.id)}>Забанить</button>
-    </p>
+    <div>
+      name: {isEdit ? <input value={name} onChange={(e) => editUser(id, "name", e)} /> : <span>{name}</span>}
+      surname: {isEdit ? <input value={surname} onChange={(e) => editUser(id, "surname", e)} /> : <span>{surname}</span>}
+      age: {isEdit ? <input type="number" value={age} onChange={(e) => editUser(id, "age", e)} /> : <span>{age}</span>}
+
+      <button onClick={() => toggleMode(id)}>{isEdit ? "save" : "edit"}</button>
+    </div>
   );
 };
 
--- src/Users.js
+++ src/Users.js
@@ -4,22 +4,35 @@ import User from "./User";
 const id = () => "_" + Math.random().toString(36).substr(2, 9); // Генератор id
 
 const initUsers = [
-  { id: id(), name: "user1", surname: "surn1", age: 30 },
-  { id: id(), name: "user2", surname: "surn2", age: 31 },
-  { id: id(), name: "user3", surname: "surn3", age: 32 },
+  { id: id(), name: "user1", surname: "surn1", age: 30, isEdit: false },
+  { id: id(), name: "user2", surname: "surn2", age: 31, isEdit: false },
+  { id: id(), name: "user3", surname: "surn3", age: 32, isEdit: false },
 ];
 
 const Users = () => {
   const [users, setUsers] = useState(initUsers);
 
-  const handleBan = (id) => {
-    setUsers(users.filter((user) => user.id !== id));
-  };
+  function toggleMode(id) {
+    setUsers(users.map(user => (user.id === id ? { ...user, isEdit: !user.isEdit } : user)));
+  }
+
+  function editUser(id, field, event) {
+    setUsers(users.map(user => (user.id === id ? { ...user, [field]: event.target.value } : user)));
+  }
 
   return (
     <div>
-      {users.map((user) => (
-        <User key={user.id} user={user} onBan={handleBan} />
+      {users.map(user => (
+        <User
+          key={user.id}
+          id={user.id}
+          name={user.name}
+          surname={user.surname}
+          age={user.age}
+          isEdit={user.isEdit}
+          toggleMode={toggleMode}
+          editUser={editUser}
+        />
       ))}
     </div>
   );
