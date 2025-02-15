f52321cd768cf7c4e7ea4c6997a368f082096d1c
Ayder <muzdabaev252@gmail.com>
2025-02-15 19:03:50 +0300

    Task 74#5

--- src/App.js
+++ src/App.js
@@ -2,36 +2,17 @@ import React, { useState } from 'react';
 
 function App() {
 	const [notes, setNotes] = useState(["Первый", "Второй", "Третий"]);
-	const [inputValue, setInputValue] = useState("");
-	const [selectedIndex, setSelectedIndex] = useState(null);
   
-	function handleLiClick(note, index) {
-	  setInputValue(note);
-	  setSelectedIndex(index);
-	}
-  
-	function handleBlur() {
-	  if (selectedIndex !== null) {
-		const updatedNotes = [...notes];
-		updatedNotes[selectedIndex] = inputValue;
-		setNotes(updatedNotes);
-		setSelectedIndex(null);
-	  }
+	function reverseList() {
+	  setNotes([...notes].reverse());
 	}
   
 	return (
 	  <div>
-		<input 
-		  type="text" 
-		  value={inputValue} 
-		  onChange={(e) => setInputValue(e.target.value)} 
-		  onBlur={handleBlur} 
-		/>
+		<button onClick={reverseList}>Перевернуть порядок</button>
 		<ul>
 		  {notes.map((note, index) => (
-			<li key={index} onClick={() => handleLiClick(note, index)}>
-			  {note}
-			</li>
+			<li key={index}>{note}</li>
 		  ))}
 		</ul>
 	  </div>
