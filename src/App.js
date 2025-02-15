614708e0550b851f45a295da11ecd405ece395e7
Ayder <muzdabaev252@gmail.com>
2025-02-15 19:16:33 +0300

    Task 76#3

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React, { useState } from 'react';
 
-import { useState } from "react";
-
 function App() {
   const [notes, setNotes] = useState([
     { first: "Первый", second: "Второй", third: "Третий" },
@@ -13,15 +11,39 @@ function App() {
     setInputs(note);
   }
 
+  function handleUpdate(index) {
+    const updatedNotes = [...notes];
+    updatedNotes[index] = inputs;
+    setNotes(updatedNotes);
+  }
+
   return (
     <div>
-      <input name="first" value={inputs.first} readOnly />
-      <input name="second" value={inputs.second} readOnly />
-      <input name="third" value={inputs.third} readOnly />
+      <div>
+        <input
+          name="first"
+          value={inputs.first}
+          onChange={(e) => setInputs({ ...inputs, first: e.target.value })}
+        />
+        <input
+          name="second"
+          value={inputs.second}
+          onChange={(e) => setInputs({ ...inputs, second: e.target.value })}
+        />
+        <input
+          name="third"
+          value={inputs.third}
+          onChange={(e) => setInputs({ ...inputs, third: e.target.value })}
+        />
+        <button onClick={() => handleUpdate(notes.findIndex((note) => note.first === inputs.first))}>
+          Обновить
+        </button>
+      </div>
+
       <ul>
         {notes.map((note, index) => (
           <li key={index}>
-            {note.first} {note.second} {note.third} 
+            {note.first} {note.second} {note.third}{" "}
             <button onClick={() => handleClick(note)}>Заполнить</button>
           </li>
         ))}
@@ -29,4 +51,5 @@ function App() {
     </div>
   );
 }
+
 export default App;
