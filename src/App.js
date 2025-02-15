d416b13c8e98c6f288f01b98fd89f9de27da61af
Ayder <muzdabaev252@gmail.com>
2025-02-15 20:00:46 +0300

    Task 83#2

--- src/App.js
+++ src/App.js
@@ -10,23 +10,17 @@ function id(){
 
 function App() {
 	const users = [
-		{id: id(), name: 'Ayder', surn: 'Muzhdabaev', age: '18'},
-		{id: id(), name: 'John', surn: 'Sent', age: '34'},
-		{id: id(), name: 'Kosa', surn: 'Nostra', age: '20'},
+		{id: id(), name: 'user1', surn: 'surn1', age: 30},
+		{id: id(), name: 'user2', surn: 'surn2', age: 31},
+		{id: id(), name: 'user3', surn: 'surn3', age: 32},
 	];
 
 	return <div>
-		<td>
-			<tr>
-				<User name={users[0].name} surn={users[0].surn} age={users[0].age} />
-			</tr>
-			<tr>
-				<User name={users[1].name} surn={users[1].surn} age={users[1].age} />
-			</tr>
-			<tr>
-				<User name={users[2].name} surn={users[2].surn} age={users[2].age} />
-			</tr>
-		</td>
+		<table>
+			<tr><User name={users[0].name} surn={users[0].surn} age={users[0].age} /></tr>
+			<tr><User name={users[1].name} surn={users[1].surn} age={users[1].age} /></tr>
+			<tr><User name={users[2].name} surn={users[2].surn} age={users[2].age} /></tr>
+		</table>
 	</div>;
 }
 
