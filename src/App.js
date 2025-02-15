19dacb2edd21001d6cc322f305846261935f66a9
Ayder <muzdabaev252@gmail.com>
2025-02-15 19:40:33 +0300

    Task 82

--- src/App.js
+++ src/App.js
@@ -4,10 +4,13 @@ import User from './User';
 import Employee from './Employee';
 
 function App() {
+	const name1 = 'Ayder';
+	const surn = 'Muzhdabaev';
+	const secn = 'Kosa-Nostra';
+	const salary = '52450$';
+
 	return <div>
-		<Employee name="test1" surn="test1" secn="test1" salary="10000$" />
-		<Employee name="test2" surn="test2" secn="test2" salary="10000$" />
-		<Employee name="test3" surn="test3" secn="test3" salary="10000$" />
+		<Employee name={name1} surn={surn} secn={secn}  salary={salary} />
 	</div>;
 }
 
