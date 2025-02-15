85eda2f9791eec5628e4c32644ee0f0be423e379
Ayder <muzdabaev252@gmail.com>
2025-02-15 19:09:09 +0300

    Task 75#2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import React, { useState } from 'react';
 
+import { useState } from "react";
+
 function App() {
   const [notes, setNotes] = useState(["Первый", "Второй", "Третий"]);
-  const [inputValue, setInputValue] = useState("");
+  const [inputs, setInputs] = useState({ first: "", second: "", third: "" });
+
+  function handleChange(e) {
+    setInputs({ ...inputs, [e.target.name]: e.target.value });
+  }
 
   function addItem() {
-    if (inputValue.trim() !== "") {
-      setNotes([...notes, inputValue]);
-      setInputValue("");
+    const newItem = `${inputs.first} ${inputs.second} ${inputs.third}`.trim();
+    if (newItem) {
+      setNotes([...notes, newItem]);
+      setInputs({ first: "", second: "", third: "" });
     }
   }
 
   return (
     <div>
-      <input 
-        type="text" 
-        value={inputValue} 
-        onChange={(e) => setInputValue(e.target.value)} 
-      />
+      <input name="first" value={inputs.first} onChange={handleChange} />
+      <input name="second" value={inputs.second} onChange={handleChange} />
+      <input name="third" value={inputs.third} onChange={handleChange} />
       <button onClick={addItem}>Добавить элемент</button>
       <ul>
         {notes.map((note, index) => (
