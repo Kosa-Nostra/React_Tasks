ed62e5ffc80927703f84420a1b1776a2ae870646
Ayder <muzdabaev252@gmail.com>
2025-02-15 19:02:44 +0300

    Task 74#4

--- src/App.js
+++ src/App.js
@@ -3,9 +3,20 @@ import React, { useState } from 'react';
 function App() {
 	const [notes, setNotes] = useState(["Первый", "Второй", "Третий"]);
 	const [inputValue, setInputValue] = useState("");
+	const [selectedIndex, setSelectedIndex] = useState(null);
   
-	function handleLiClick(note) {
+	function handleLiClick(note, index) {
 	  setInputValue(note);
+	  setSelectedIndex(index);
+	}
+  
+	function handleBlur() {
+	  if (selectedIndex !== null) {
+		const updatedNotes = [...notes];
+		updatedNotes[selectedIndex] = inputValue;
+		setNotes(updatedNotes);
+		setSelectedIndex(null);
+	  }
 	}
   
 	return (
@@ -14,10 +25,11 @@ function App() {
 		  type="text" 
 		  value={inputValue} 
 		  onChange={(e) => setInputValue(e.target.value)} 
+		  onBlur={handleBlur} 
 		/>
 		<ul>
 		  {notes.map((note, index) => (
-			<li key={index} onClick={() => handleLiClick(note)}>
+			<li key={index} onClick={() => handleLiClick(note, index)}>
 			  {note}
 			</li>
 		  ))}
@@ -25,5 +37,4 @@ function App() {
 	  </div>
 	);
   }
-  
 export default App;
