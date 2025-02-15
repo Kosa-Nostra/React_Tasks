20e73fc2fde042f5d9795701abdda260e572bf7c
Ayder <muzdabaev252@gmail.com>
2025-02-15 19:08:14 +0300

    Task 75#1

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
 import React, { useState } from 'react';
 
 function App() {
-	const [notes, setNotes] = useState(["Первый", "Второй", "Третий"]);
-  
-	function reverseList() {
-	  setNotes([...notes].reverse());
-	}
-  
-	return (
-	  <div>
-		<button onClick={reverseList}>Перевернуть порядок</button>
-		<ul>
-		  {notes.map((note, index) => (
-			<li key={index}>{note}</li>
-		  ))}
-		</ul>
-	  </div>
-	);
+  const [notes, setNotes] = useState(["Первый", "Второй", "Третий"]);
+  const [inputValue, setInputValue] = useState("");
+
+  function addItem() {
+    if (inputValue.trim() !== "") {
+      setNotes([...notes, inputValue]);
+      setInputValue("");
+    }
   }
+
+  return (
+    <div>
+      <input 
+        type="text" 
+        value={inputValue} 
+        onChange={(e) => setInputValue(e.target.value)} 
+      />
+      <button onClick={addItem}>Добавить элемент</button>
+      <ul>
+        {notes.map((note, index) => (
+          <li key={index}>{note}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
 export default App;
