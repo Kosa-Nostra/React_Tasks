a619c6876637edd4abcce166f15aa2538adef82c
Ayder <muzdabaev252@gmail.com>
2025-02-15 19:26:56 +0300

    Task 79

--- src/App.js
+++ src/App.js
@@ -1,50 +1,12 @@
-import React, { useState } from 'react';
-
-const id = () => '_' + Math.random().toString(36).substr(2, 9); // Простой генератор id
-
-const initNotes = [
-  {
-    id: id(),
-    name: 'name1',
-    desc: 'long description 1',
-    show: false,
-  },
-  {
-    id: id(),
-    name: 'name2',
-    desc: 'long description 2',
-    show: false,
-  },
-  {
-    id: id(),
-    name: 'name3',
-    desc: 'long description 3',
-    show: false,
-  },
-];
+import React from 'react';
+import Product from './Product'
+import User from './User';
 
 function App() {
-  const [notes, setNotes] = useState(initNotes);
-
-  const toggleDescription = (id) => {
-    setNotes(prevNotes =>
-      prevNotes.map(note =>
-        note.id === id ? { ...note, show: !note.show } : note
-      )
-    );
-  };
-
-  const result = notes.map(note => (
-    <p key={note.id}>
-      {note.name},
-      {note.show && <i>{note.desc}</i>}
-      <button onClick={() => toggleDescription(note.id)}>
-        {note.show ? 'Скрыть описание' : 'Показать описание'}
-      </button>
-    </p>
-  ));
-
-  return <div>{result}</div>;
+	return <div>
+		<Product />
+		<User />
+	</div>;
 }
 
 export default App;
---
+++ src/User.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function User() {
+    return <p>
+       ТУТ БУДУТ ПОЛЬЗОВАТЕЛИ 
+    </p>;
+}
+
+export default User;
