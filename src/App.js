100a4806a8937ffd0d1b3f0ae233bdfe800bd54e
Ayder <muzdabaev252@gmail.com>
2025-02-17 19:19:12 +0300

    Task 95#1

--- src/App.js
+++ src/App.js
@@ -6,6 +6,34 @@ function id(){
 	const randomIdsUUID = Array.from({length: 5}, () => uuidv4());
 }
 
+function TempInp({ temp, setTemp }) {
+	return (
+		<div>
+			<label>
+				Введите температуру:
+				<input
+					type="number"
+					value={temp}
+					onChange={(event) => setTemp(event.target.value)}
+				/>
+			</label>
+		</div>
+	);
+}
+
+function Verdict({ temp }) {
+	let message;
+	if (temp < 0) {
+		message = "Вода твердая (лед)";
+	} else if (temp >= 100) {
+		message = "Вода газообразная (пар)";
+	} else {
+		message = "Вода жидкая";
+	}
+
+	return <p>{message}</p>;
+}
+
 function App() {
   return (
     <div>
