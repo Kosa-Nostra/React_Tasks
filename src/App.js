44f9c95cc170741c444604ffe4be8809780b31ae
Ayder <muzdabaev252@gmail.com>
2025-02-18 10:08:35 +0300

    Task 98

--- src/App.js
+++ src/App.js
@@ -1,47 +1,15 @@
 import React from 'react';
-import Users from './Users';
-import {v4 as uuidv4} from 'uuid'
-
-function id(){
-	const randomIdsUUID = Array.from({length: 5}, () => uuidv4());
-}
+import './styles.css';
 
 
 
 function App() {
-	const [notes, setNotes] = useState([1, 2, 3, 4, 5]);
-	const [editNum, setEditNum] = useState(null);
-	const [value, setValue] = useState('');
-
-	function startEdit(index) {
-		setEditNum(index);
-		setValue(notes[index]);
-	}
-
-	function saveItem() {
-		if (editNum !== null) {
-			const updatedNotes = [...notes];
-			updatedNotes[editNum] = value;
-			setNotes(updatedNotes);
-			setEditNum(null); // Выходим из режима редактирования
-		}
-	}
-
+	const a11 = <p>text</p>
 	return (
-		<div>
-			{notes.map((note, index) => (
-				<p key={index} onClick={() => startEdit(index)}>
-					{note}
-				</p>
-			))}
-
-			{editNum !== null && (
-				<div>
-					<input value={value} onChange={(e) => setValue(e.target.value)} />
-					<button onClick={saveItem}>Сохранить</button>
-				</div>
-			)}
-		</div>
+		<div class="class1">
+	<button class="class2" onClick={a11}>btn1</button>
+	<button class="class3" onClick={a11}>btn1</button>
+</div>	
 	);
 }
 
