93697836de98d6dd8b00393d247db0af34cb7e90
Ayder <muzdabaev252@gmail.com>
2025-02-18 10:14:23 +0300

    Task 100

--- src/App.js
+++ src/App.js
@@ -1,39 +1,17 @@
 import React from 'react';
 import './styles.css';
+import { styles } from "./styles";
 
 
 
 function App() {
-	const class1 = {
-		width: '200px',
-		border: '2px solid brown',
-		padding: '10px',
-		textAlign: 'center',
-	};
-
-	const class2 = {
-		color: 'orangered',
-		fontWeight: 'bold',
-	};
-
-	const class3 = {
-		fontStyle: 'italic',
-		color: 'brown',
-	};
-
-	const class4 = {
-		backgroundColor: 'orange',
-		fontWeight: 'bold',
-		color: 'white',
-	};
-
 	return (
-		<div style={class1}>
-			<p style={class2}>text</p>
-			<p style={class3}>text</p>
-			<p style={class4}>text</p>
+		<div style={styles.class1}>
+			<p style={styles.class2}>text</p>
+			<p style={styles.class3}>text</p>
+			<p style={styles.class4}>text</p>
 		</div>
 	);
-} 
+}  
 export default App;
 
