a5229fe0daa460b960ddba7893cf8737607caf24
Ayder <muzdabaev252@gmail.com>
2025-02-18 10:11:28 +0300

    Task 99

--- src/App.js
+++ src/App.js
@@ -4,14 +4,36 @@ import './styles.css';
 
 
 function App() {
-	const a11 = <p>text</p>
-	return (
-		<div class="class1">
-	<button class="class2" onClick={a11}>btn1</button>
-	<button class="class3" onClick={a11}>btn1</button>
-</div>	
-	);
-}
+	const class1 = {
+		width: '200px',
+		border: '2px solid brown',
+		padding: '10px',
+		textAlign: 'center',
+	};
 
+	const class2 = {
+		color: 'orangered',
+		fontWeight: 'bold',
+	};
+
+	const class3 = {
+		fontStyle: 'italic',
+		color: 'brown',
+	};
+
+	const class4 = {
+		backgroundColor: 'orange',
+		fontWeight: 'bold',
+		color: 'white',
+	};
+
+	return (
+		<div style={class1}>
+			<p style={class2}>text</p>
+			<p style={class3}>text</p>
+			<p style={class4}>text</p>
+		</div>
+	);
+} 
 export default App;
 
